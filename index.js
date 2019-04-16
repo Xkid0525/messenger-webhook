90848cd0cec75b790bcc635eb65598a82e1a10df
Shrabin Tuladhar <tdr.shrabin@gmail.com>
2019-04-16 12:44:23 +0545

    initail9

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ app.post("/webhook", (req, res) => {
   if (body.object === "page") {
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
 
     // Returns a '200 OK' response to all requests
@@ -49,13 +52,4 @@ app.get("/webhook", (req, res) => {
       res.sendStatus(403);
     }
   }
-});
-body.entry.forEach(function(entry) {
-  // Gets the body of the webhook event
-  let webhook_event = entry.messaging[0];
-  console.log(webhook_event);
-
-  // Get the sender PSID
-  let sender_psid = webhook_event.sender.id;
-  console.log("Sender PSID: " + sender_psid);
 });
